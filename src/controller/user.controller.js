45e78d76889e92dca975aadb84f20ccbf127e208
mafqla <1692285748@qq.com>
2022-05-15 23:23:17 +0800

    添加查询用户api(缺少权限)

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const { User } = require('../model/index')
+const { User, Role } = require('../model/index')
 // 导入 bcryptjs 这个包
 const bcrypt = require('bcryptjs')
 
@@ -38,60 +38,84 @@ const register = async (ctx) => {
       ctx.body = {
         success: false,
         code: 500,
+        message: '注册出现异常',
+        err: err.message,
+      }
+    })
+}
+
+const login = async (ctx) => {
+  const { username, password } = ctx.request.body
+
+  await User.findOne({ username })
+    .then(async (result) => {
+      if (!result) {
+        ctx.body = {
+          success: false,
+          code: 300,
+          message: '用户不存在',
+        }
+      } else {
+        console.log(result.username)
+        // 密码验证
+        const isPasswordValid = bcrypt.compareSync(password, result.password)
+        // console.log(isPasswordValid)
+        if (isPasswordValid) {
+          const token = jwt.sign(
+            { username: result.username, id: result.id },
+            'imooc-api',
+          )
+          ctx.body = {
+            success: true,
+            code: 200,
+            data: {
+              token: token,
+            },
+            message: '登录成功',
+          }
+        } else {
+          ctx.body = {
+            success: false,
+            code: 300,
+            message: '密码错误',
+          }
+        }
+      }
+    })
+    .catch((err) => {
+      ctx.body = {
+        success: false,
+        code: 500,
         message: '登录出现异常',
         err: err.message,
       }
     })
 }
-const login = async (ctx) => {
-  const { username, password } = ctx.request.body
-  // const isPasswordValid = bcrypt.compareSync(password, User.password)
-
-  await User.findOne({ username }).then(async (result) => {
-    if (!result) {
-      ctx.body = {
-        success: false,
-        code: 300,
-        message: '用户不存在',
-      }
-    } else {
-      // console.log(result.username)
-      // 密码验证
-      const isPasswordValid = bcrypt.compareSync(password, result.password)
-      // console.log(isPasswordValid)
-      if (isPasswordValid) {
-        const token = jwt.sign(
-          { username: result.username, id: result.id },
-          'imooc-api',
-        )
-        ctx.body = {
-          success: true,
-          code: 200,
-          data: {
-            token: token,
-          },
-          message: '登录成功',
-        }
-      } else {
-        ctx.body = {
-          success: false,
-          code: 300,
-          message: '密码错误',
-        }
-      }
-    }
-  })
-}
 
 //获取用户信息
 const profile = async (ctx) => {
-  await User.find().then((result) => {
-    ctx.body = {
-      success: true,
-      code: 200,
-      data: result,
-      message: '获取用户信息成功！',
-    }
+  await User.findOne().then(async (result) => {
+    // console.log(result)
+    const roleId = result.roleId
+    await Role.findOne({ roleId }).then(async (role) => {
+      ctx.body = {
+        success: true,
+        code: 200,
+        data: {
+          role: {
+            id: role.id,
+            title: role.title,
+          },
+          _id: result._id,
+          id: result.id,
+          username: result.username,
+          // title: role.title,
+          avtar: result.avatar,
+          // permission,
+        },
+        message: '获取用户信息成功！',
+      }
+    })
   })
 }
 
--- src/model/index.js
+++ src/model/index.js
@@ -2,16 +2,31 @@ const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema(
   {
-    id: Number,
+    id: String,
+    roleId: Number,
+    openTime: String,
     username: String,
     password: {
       type: String,
     },
+    mobile: String,
+    avatar: String,
   },
   { versionKey: false },
 )
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('Userinfo', userSchema, 'userinfo')
+
+const roleSchema = new mongoose.Schema(
+  {
+    id: Number,
+    title: String,
+    descibe: String,
+  },
+  { versionKey: false },
+)
+
+const Role = mongoose.model('Role',  roleSchema, 'role')
 
 module.exports = {
-  User,
+  User,Role
 }
